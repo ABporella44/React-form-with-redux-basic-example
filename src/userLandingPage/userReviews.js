4c22d137128674bdd622a7c0aa6b57a5bafc510c
Abhilash Porella <aporella@keypoint-tech.com>
2021-11-08 16:39:13 +0530

    Added map function

--- src/userLandingPage/userReviews.js
+++ src/userLandingPage/userReviews.js
@@ -2,21 +2,52 @@ import React from "react";
 import { Paper } from '@material-ui/core';
 import "../App.css"
 
+
 class Userreviews extends React.Component{
     constructor(props){
        super(props);
+       this.state={
+           reviewerInfo:[
+               { 
+                image:"./profile.jpg",
+                review:"Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry's standard dummy text ever since th",
+                name:"ramesh",
+                designation:"Fullstack Developer"
+             },
+
+           /* { 
+               image:"./another.jpg",
+               review:"Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry's standard dummy text ever since th",
+               name:"Bala",
+               designation:"Backend Developer"
+            },
+ 
+            { 
+               image:"./another2.jpg",
+               review:"Lorem Ipsum is simply dummy text of the printing and typesettingindustry. Lorem Ipsum has been the industry's standard dummy text ever since th",
+               name:"mrudhula",
+               designation:"Backend Developer"
+            },*/
+        ],
+        reviewCount:0,
+       }
     }
     render(){
+        
         return(
             <div>
-                <Paper>
                 <div className="user-image-div">
-                <img src={'./profile.jpg'} className="user-image-class" alt="author"></img>
+                 {this.state.reviewerInfo.map((item,index)=>
+                   <Paper elevation={4} className="review-elements">
+                   <div className="user-image">
+                     <img src={`${item.image}`} className="user-image-class" alt="author"></img>
+                   </div>
+                   <div className="user-review">
+                     <p className="review-text">{item.review}<span className="reviewer-name">.......{item.name}({item.designation})</span></p>
+                   </div>
+                  </Paper>
+                 )}       
                 </div>
-                <div className="user-review">
-                    <p className="review-text">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since th"</p>
-                </div>
-                </Paper>
             </div>)
     }
 }
